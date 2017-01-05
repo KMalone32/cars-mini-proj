be56675f39416e42d76128ea4b34c8d4e4f7ed02
William Cox <willubyu7@gmail.com>
2017-01-05 12:29:26 -0700

    added session functionality

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var config = require('./config.json');
 
 var carCtrl = require('./controller/carCtrl.js');
+var cartCtrl = require('./controller/cartCtrl.js');
 
 var app = express();
 
 app.use(bodyParser.json());
+app.use(session({
+  secret: config.sessionSecret,
+  saveUninitialized: false,
+  resave: false
+}));
 
 app.get('/cars', carCtrl.index);
 app.get('/cars/:id', carCtrl.show);
 app.post('/cars', carCtrl.create);
 app.put('/cars/:id', carCtrl.update);
 app.delete('/cars/:id', carCtrl.destroy);
+
+app.get('/cart', cartCtrl.index);
+app.post('/cart', cartCtrl.create);
 
 var port = 3001;
 app.listen(port, function() {
